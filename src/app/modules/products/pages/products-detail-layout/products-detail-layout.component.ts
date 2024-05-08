c7d281249c82e90d255f96bc1c05186a25e9af72
gcarreno <gcarreno@pagochinchin.com>
2024-05-08 16:52:30 -0400

    detalle de producto

--- src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
+++ src/app/modules/products/pages/products-detail-layout/products-detail-layout.component.ts
@@ -18,65 +18,16 @@ export class ProductsDetailLayoutComponent {
       images.product6,
     ],
     mainImage: images.product6,
-    name: 'Never split the difference',
-    brand: 'Chris Voss',
-    price: '175.00',
+    name: 'PASTA PLUMA SUPERIOR',
+    brand: 'MARY',
+    price: '1,75',
     quantity: 1,
     currency: 'S',
     aditionalInfo: [
       {
-        title: 'sinopsis',
-        texts: [
-          'El prestigioso ex negociador internacional del FBI Chris Voss, especializado en secuestros con rehenes, nos enseña un método de negociación rompedor: tácticas para negociaciones duras que son aplicables en múltiples aspectos de nuestras vidas.',
-
-          // 'Rompe la barrera del no es un manual de negociación imprescindible desarrollado y perfeccionado a lo largo de la extraordinaria carrera de Chris Voss como negociador en secuestros con rehenes y como reconocido profesor en las escuelas de negocio más prestigiosas del mundo. Voss ha puesto a prueba estas técnicas en todo tipo de situaciones y ha comprobado su efectividad, tanto en los inicios de su carrera cuando patrullaba las peligrosas calles de Kansas City como en los cursos que imparte en las mejores universidades. Entre las muchas y distintas personas que han aplicado sus enseñanzas, se encuentran desde clientes que han visto aumentar significativamente los beneficios de sus empresas hasta estudiantes de programas de MBA que han conseguido empleos mejores o incluso padres que tienen que vérselas con sus hijos.',
-
-          // 'Rompe la barrera del no relata los entresijos de situaciones verdaderamente dramáticas y revela las mejores y más eficaces estrategias de negociación. Voss, uno de los mejores negociadores del mundo, nos enseña a emplear estas técnicas en cualquier aspecto de nuestra vida.'
-        ]
-      },
-      // {
-      //   title: 'otra cosa',
-      //   text: 'El prestigioso ex negociador internacional del FBI Chris Voss, especializado en secuestros con rehenes, nos enseña un método de negociación rompedor: tácticas para negociaciones duras que son aplicables en múltiples aspectos de nuestras vidas. Rompe la barrera del no es un manual de negociación imprescindible desarrollado y perfeccionado a lo largo de la extraordinaria carrera de Chris Voss como negociador en secuestros con rehenes y como reconocido profesor en las escuelas de negocio más prestigiosas del mundo. Voss ha puesto a prueba estas técnicas en todo tipo de situaciones y ha comprobado su efectividad, tanto en los inicios de su carrera cuando patrullaba las peligrosas calles de Kansas City como en los cursos que imparte en las mejores universidades. Entre las muchas y distintas personas que han aplicado sus enseñanzas, se encuentran desde clientes que han visto aumentar significativamente los beneficios de sus empresas hasta estudiantes de programas de MBA que han conseguido empleos mejores o incluso padres que tienen que vérselas con sus hijos. Rompe la barrera del no relata los entresijos de situaciones verdaderamente dramáticas y revela las mejores y más eficaces estrategias de negociación. Voss, uno de los mejores negociadores del mundo, nos enseña a emplear estas técnicas en cualquier aspecto de nuestra vida.'
-      // },
-      // {
-      //   title: 'COLOR',
-      //   type: 'variants',
-      //   colorVariants: [
-      //     {
-      //       color: '#fff'
-      //     },
-      //     {
-      //       color: '#000'
-      //     },
-      //     {
-      //       color: '#148954'
-      //     }
-      //   ]
-      // },
-      // {
-      //   title: 'Tallas',
-      //   type: 'variants',
-      //   textVariants: [
-      //     {
-      //       text: 'XS'
-      //     },
-      //     {
-      //       text: 'S'
-      //     },
-      //     {
-      //       text: 'M'
-      //     },
-      //     {
-      //       text: 'L'
-      //     },
-      //     {
-      //       text: 'XL'
-      //     },
-      //     {
-      //       text: 'XXL'
-      //     },
-      //   ]
-      // },
+        title: 'SKU',
+        text: '48234829449493'
+      }
     ],
   }
 
@@ -122,7 +73,7 @@ export class ProductsDetailLayoutComponent {
 
   /**
    * Me lleva a la vista de detalle
-   * @param id 
+   * @param id
    */
   navigate(id: string) {
     this.router.navigate(['/products/detail'])
